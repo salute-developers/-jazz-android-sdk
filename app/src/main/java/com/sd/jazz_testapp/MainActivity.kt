2a6d4b6f4a5661a40ea856b6a151e55579cc6b27
Звягов Евгений Дмитриевич [B] <EDZvyagov@sberbank.ru>
2024-06-21 18:30:06 +0300

    Release Jazz Sdk 24.03.1

--- app/src/main/java/com/sd/jazz_testapp/MainActivity.kt
+++ app/src/main/java/com/sd/jazz_testapp/MainActivity.kt
@@ -6,35 +6,18 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.sd.jazz_testapp.databinding.ActivityMainBinding
-import com.sdkit.jazz.client.integration.api.domain.JazzTokenProvider
 import com.sdkit.jazz.client.integration.api.model.AudioDevice
 import com.sdkit.jazz.client.integration.api.model.ConferenceConnectionArguments
 import com.sdkit.jazz.client.integration.api.model.CreateVideoCallArguments
-import com.sdkit.jazz.client.integration.api.model.JazzTokenConfiguration
 import com.sdkit.jazz.client.integration.api.model.JoinVideoCallArguments
 import com.sdkit.jazz.client.integration.api.model.ScheduledConferenceResult
 import com.sdkit.jazz.sdk.di.JazzSdk
-import com.sdkit.jazz.sdk.di.JazzSdkTokenProvider
-import com.sdkit.jazz.sdk.di.JazzTokenConfigurationProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
-    // Что бы получить ваш ключ, пройдите сюда -> https://clck.ru/35aWZw
-    // Так же добавить этот JazzTokenProvider можно в DefaultJazzPlatformDependencies,
-    // которые прокидываются в MainApplication
-    val jazzSdkTokenProvider = JazzSdkTokenProvider(provider = object : JazzTokenConfigurationProvider {
-        override fun getConfiguration(): JazzTokenConfiguration {
-            return JazzTokenConfiguration(
-                userId = "test",
-                secretKey = binding.sdkKeyEditText.text.toString(),
-                liveTimeDurationInSeconds = 180
-            )
-        }
-    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         binding.sdkKeyDocLinkTextView.movementMethod = LinkMovementMethod.getInstance()
 
         binding.createConferenceButton.setOnClickListener {
+
+            (application as? MainApplication)?.setSecretKey(binding.sdkKeyEditText.text.toString())
+
             JazzSdk.getIntegrationClientApi().jazzIntegrationClient.createConference(
                 arguments = CreateVideoCallArguments(
                     roomType = "MEETING",
@@ -58,11 +44,6 @@ class MainActivity : AppCompatActivity() {
                     summarizationEnabled = null,
                     autoRecord = null,
                     audioDevice = AudioDevice.DEFAULT,
-                    jazzTokenProvider = object : JazzTokenProvider {
-                        override suspend fun getToken(): String? {
-                            return jazzSdkTokenProvider.getToken()
-                        }
-                    }
                 ),
             )
         }
@@ -84,11 +65,6 @@ class MainActivity : AppCompatActivity() {
                     lobbyEnabled = false,
                     autoRecord = false,
                     summarizationEnabled = false,
-                    jazzTokenProvider = object : JazzTokenProvider {
-                        override suspend fun getToken(): String? {
-                            return jazzSdkTokenProvider.getToken()
-                        }
-                    }
                 )
             }
 
